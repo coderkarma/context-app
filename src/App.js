c804e31bffa9d87b7d07f6e1edae0f404c2edbe9
Karma Drukpa <karmadrukpa11@gmail.com>
2020-05-21 19:09:32 -0700

    Add PageContext

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import Navbar from './Navbar'
-function App() {
-  return (
-    <Navbar/>
-  );
-}
+import Navbar from './Navbar';
+import Form from './Form';
+import PageContent from './PageContent';
+import { ThemeProvider } from './contexts/ThemeContext';
+
+const App = () => {
+	return (
+		<ThemeProvider>
+			<PageContent>
+				<Navbar />
+				<Form />
+			</PageContent>
+		</ThemeProvider>
+	);
+};
 
 export default App;
