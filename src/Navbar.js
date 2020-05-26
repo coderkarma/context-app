4a7ab8ab0606b065c65eea913552f6db26977ff9
Karma Drukpa <karmadrukpa11@gmail.com>
2020-05-26 14:55:39 -0700

    Add two context in one component

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,6 +9,22 @@ import SearchIcon from '@material-ui/icons/Search';
 import Switch from '@material-ui/core/Switch';
 import styles from './styles/NavBarStyles';
 import { ThemeContext } from './contexts/ThemeContext';
+import { withLanguageContext } from './contexts/LanguageContext';
+
+const content = {
+	english: {
+		search: 'Search',
+		flag: '🇬🇧',
+	},
+	french: {
+		search: 'Chercher',
+		flag: '🇫🇷',
+	},
+	spanish: {
+		search: 'Buscar',
+		flag: '🇪🇸',
+	},
+};
 
 class Navbar extends Component {
 	static contextType = ThemeContext;
@@ -16,37 +32,43 @@ class Navbar extends Component {
 	render() {
 		const { isDarkMode, toggleTheme } = this.context;
 		const { classes } = this.props;
+		const { language } = this.props.languageContext;
+		const { search, flag } = content[language];
 		return (
-			<AppBar
-				className={classes.root}
-				color={isDarkMode ? 'default' : 'primary'}>
-				<Toolbar>
-					<IconButton className={classes.menuButton} color='inherit'>
-						<span>🇫🇷</span>
-					</IconButton>
-					<Typography
-						className={classes.title}
-						variant='h6'
-						color='inherit'>
-						App Title
-					</Typography>
-					<Switch onChange={toggleTheme} />
-					<div className={classes.grow} />
-					<div className={classes.search}>
-						<div className={classes.searchIcon}>
-							<SearchIcon />
+			<div className={classes.root}>
+				<AppBar
+					className={classes.root}
+					color={isDarkMode ? 'default' : 'primary'}>
+					<Toolbar>
+						<IconButton
+							className={classes.menuButton}
+							color='inherit'>
+							<span>{flag}</span>
+						</IconButton>
+						<Typography
+							className={classes.title}
+							variant='h6'
+							color='inherit'>
+							App Title
+						</Typography>
+						<Switch onChange={toggleTheme} />
+						<div className={classes.grow} />
+						<div className={classes.search}>
+							<div className={classes.searchIcon}>
+								<SearchIcon />
+							</div>
+							<InputBase
+								placeholder={`${search}...`}
+								classes={{
+									root: classes.inputRoot,
+									input: classes.inputInput,
+								}}
+							/>
 						</div>
-						<InputBase
-							placeholder='Search...'
-							classes={{
-								root: classes.inputRoot,
-								input: classes.inputInput,
-							}}
-						/>
-					</div>
-				</Toolbar>
-			</AppBar>
+					</Toolbar>
+				</AppBar>
+			</div>
 		);
 	}
 }
-export default withStyles(styles)(Navbar);
+export default withLanguageContext(withStyles(styles)(Navbar));
